4131814356345e2b27149187ef8cd1144d395d1d
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-09-01 13:10:06 +0800

    Initialize Repo
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Shimakaze.Sdk.Example
+这是一个示例项目，它主要用于展示如何通过Shimakaze.Sdk将模组构建自动化
+
+# 环境准备
+- Phobos需要MSVC142和ATL库
+- XNA-CNCNet-Client需要.Net 7 SDK
+- dta-mg-client-launcher需要.Net Framework 4.7.1 SDK
---
+++ build.ps1
@@ -0,0 +1,33 @@
+$pfbase = [System.Environment]::Is64BitOperatingSystem ? ${env:ProgramFiles(x86)} : $env:ProgramFiles
+$vswhere = "$pfbase\Microsoft Visual Studio\Installer\vswhere.exe"
+$msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
+
+Remove-Item out -Recurse -Force | Out-Null
+New-Item -Type Directory out | Out-Null
+
+# Build Phobos
+& $msbuild Phobos\Phobos.sln -p:Configuration=Release -v:m
+if ($LASTEXITCODE -ne 0) {
+    throw "Phobos Build Failed."
+}
+Move-Item Phobos\Release\Phobos.* out\ -Force
+
+# Build XNA-CNCNet-Client
+. Client\BuildScripts\Build-Ares.ps1
+Move-Item Client\Compiled\Ares\Resources out\ -Force
+
+# Build dta-mg-client-launcher
+dotnet build ClientLauncher -c:Release
+if ($LASTEXITCODE -ne 0) {
+    throw "ClientLauncher Build Failed."
+}
+Move-Item ClientLauncher\bin\Release\net471\* out\ -Force
+
+# Build Mod
+# TODO: This is a BUG!
+Remove-Item Mod\bin, Mod\obj -Recurse -Force | Out-Null
+dotnet pack Mod -c:Release
+if ($LASTEXITCODE -ne 0) {
+    throw "Mod Build Failed."
+}
+Move-Item Mod\bin\Release\*.mix out\ -Force
